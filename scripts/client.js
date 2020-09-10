b527833a81f5ff1a17107152145bf6cb35ddaf8a
Myron <dubtwee@gmail.com>
2020-09-10 17:04:30 -0500

    initial codebase

---
+++ scripts/client.js
@@ -0,0 +1,39 @@
+console.log('Hello Castor');
+
+//
+// EQUALS OPERATOR
+// ==============================
+
+
+//
+// LOOPS & ARRAYS
+// ==============================
+
+//
+// How many items are in our array?
+// ------------------------------
+
+// What item is in our array?
+
+// what is the first item?
+// what is the last item?
+
+//
+// LOOP TYPE: for i
+// ------------------------------
+// console.log('\nSTART OF for i loop ::');
+
+//
+// LOOP TYPE: for in
+// ------------------------------
+// console.log('\nSTART OF for in loop ::');
+
+//
+// LOOP TYPE: for of
+// ------------------------------
+// console.log('\nSTART OF for in loop ::');
+
+//
+// LOOP TYPE: while
+// ------------------------------
+// console.log('\nSTART OF while loop ::');
